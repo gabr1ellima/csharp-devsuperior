862c806b81b710d33c9d1130773a277b1990705b
gabr1ellima <gabriellima0409@gmail.com>
2024-02-08 09:59:32 -0300

    Section 02 - Intro C# e .NET

---
+++ 02-intro-csharp-dotnet/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // Referencia para o namespace chamado system, padrao do dotnet e contem funcoes basicas de manipulacao de programas, opcional
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
